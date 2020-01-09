e35a30e48b6a5791e0fb47cafc59884580cb5192
designervoid <newluciocheg@gmail.com>
2020-01-09 15:40:15 +0300

    rename components

---
+++ src/views/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Index/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import IndexComponent from '@/components/IndexComponent.vue'
+
+export default {
+  name: 'index',
+  components: {
+    Index
+  }
+}
+</script>
