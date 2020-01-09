bdf2b339dacbf0db0de7861acd43951f6aa288a6
designervoid <newluciocheg@gmail.com>
2020-01-09 15:53:07 +0300

    install buefy, fontawesome.

--- src/components/IndexComponent.vue
+++ src/components/IndexComponent.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    test
+    <div class="columns">
+      <div class="column is-mobile is-2">
+        test is-2
+      </div>
+      <div class="column is-mobile is-10">
+        test is-10
+      </div>
+    </div>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Buefy from "buefy";
+
+import "./main.scss";
 
 Vue.config.productionTip = false
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+// internal icons
+import { faGlobe, faCog, faCubes, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faGlobe, faCog, faCubes, faSignOutAlt);
+Vue.component('vue-fontawesome', FontAwesomeIcon);
+Vue.use(Buefy, {
+  defaultIconComponent: 'vue-fontawesome',
+  defaultIconPack: 'fas',
+});
 
 new Vue({
   router,
